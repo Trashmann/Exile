81123dc5faed2898dbb28dc5fba037741b3c3c70
Trash_mann <reosliquidbards@hotmail.com>
2016-12-07 20:55:42 -0330

    Added tiles

--- tile.lua
+++ tile.lua
@@ -7,13 +7,13 @@ local TILE_HEIGHT = 32
 local TILESETWIDTH, TILESETHEIGHT = Tileset:getWidth(), Tileset:getHeight()
 
 
-Grass = love.graphics.newQuad(0,0,TILE_WIDTH, TILE_HEIGHT, TILESETWIDTH, TILESETHEIGHT)
+Grass = love.graphics.newQuad(0,-2,TILE_WIDTH, TILE_HEIGHT, TILESETWIDTH, TILESETHEIGHT)
 Dirt = love.graphics.newQuad(32,0,TILE_WIDTH, TILE_HEIGHT, TILESETWIDTH, TILE_HEIGHT)
 end
 
 
 
 function TILES_DRAW()
-love.graphics.draw(Tileset, Grass, 368, 268)
+love.graphics.draw(Tileset, Grass, 300, 300)
 
 end
