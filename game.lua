b3ff8d6e49dfaf2cc09e8a5264fbbb68fb07f3be
Trash_mann <reosliquidbards@hotmail.com>
2016-12-07 20:57:43 -0330

    HEllo

--- game.lua
+++ game.lua
@@ -1,43 +1,17 @@
+game = {}
 
 function GAME_MAIN()
-print("This is working :D")
-width = tonumber("800")
-height = tonumber("600")
-love.window.setMode(width, height, null)
-
-
---Calls
-WORKERS_MAIN()
 
 end
 
 function GAME_UPDATE(dt)
-WORKERS(dt)
+
 end
+
 
 function GAME_DRAW()
-WORKERS_DRAW()
+
 end
 
 
-function WORKERS_MAIN()
-worker = {}
-workerwidth = 16
-workerheight = 16
-workery = 10
-workerx = 10
-workerfacing = 'RIGHT'
-workerspeed = 10
-end
 
-
-function WORKERS(dt)
-if workerfacing == 'RIGHT' then do
-workers.y = workerspeed * dt 
-end
-end
-end 
-
-function WORKERS_DRAW()
-love.graphics.rectangle('fill', workerx, workery, workerwidth, workerheight)
-end
